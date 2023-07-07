31e78653281a25604fa634ac9d4398e8a3d8e5aa
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2023-07-07 19:33:54 +0200

    limpieza3

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import cors from "cors";
 import { handler } from "./frontend/build/handler.js";
 import express from "express";
-import { loadBackend_jorge_v1 } from './backend/jfs/modulo-jfs_v1.js';
-import { loadBackend_jorge_v2 } from './backend/jfs/modulo-jfs_v2.js';
-import { loadBackend_jorge_v3 } from './backend/jfs/modulo-jfs_v3.js';
 import { LoadModulo_Pablo } from './backend/pvl/modulo-pvl.js';
 import { LoadModulo_Pablo_v2 } from './backend/pvl/modulo-pvl-v2.js';
-import { loadBackend_src_v2 } from './backend/src/modulo-src_v2.js';
-//import { LoadModulo_Pablo } from './backend/pvl/modulo-pvl.js';
 
 var app = express();
 
@@ -16,12 +11,8 @@ app.use(cors());
 var port = process.env.PORT || 12345;
 app.use(express.json());
 //BACKEND
-loadBackend_jorge_v1(app);
-loadBackend_jorge_v2(app);
-loadBackend_jorge_v3(app);
 LoadModulo_Pablo(app);
 LoadModulo_Pablo_v2(app);
-loadBackend_src_v2(app);
 
 app.use(handler); 
 
